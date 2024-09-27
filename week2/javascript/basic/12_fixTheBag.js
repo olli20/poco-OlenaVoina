4e30159d05dfdbf248733d800c6015d0034452dc
olli20 <olena.voina@powercoders.org>
2024-09-27 12:05:09 +0200

    Ex. 4.1.12 (*) - Fix the code

--- week2/javascript/basic/12_fixTheBag.js
+++ week2/javascript/basic/12_fixTheBag.js
@@ -10,3 +10,19 @@
 // 1 incrementItems ([0 , 1, 2 , 3]) -> [1 , 2 , 3 , 4]
 // 2 incrementItems ([2 , 4, 6 , 8]) -> [3 , 5 , 7 , 9]
 // 3 incrementItems ([ -1 , -2 , -3 , -4]) -> [0 , -1 , -2 , -3]
+
+function incrementItems (array) {
+    for ( let i = 0; i < array.length ; i++) {
+        array[i] = array[i] + 1;
+    }
+    
+    return array;
+}
+
+const simpleArray = [0 , 1, 2 , 3];
+
+console.log("Array before running the incrementItems function: ", simpleArray);
+
+incrementItems(simpleArray);
+
+console.log("Array after running the incrementItems function: ", simpleArray);
