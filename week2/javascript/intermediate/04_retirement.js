787a80bb9ee88eff0a24722590248bd72b462b83
olli20 <olena.voina@powercoders.org>
2024-09-28 18:33:46 +0200

    add 4.2.5 Sum and Average, improved 4.2.4 Retirement

--- week2/javascript/intermediate/04_retirement.js
+++ week2/javascript/intermediate/04_retirement.js
@@ -1,30 +1,32 @@
+// Retirement calculator
+// To run in CLI: node 04_retirement.js
 
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
   
-  // aks the question
-  const promptUser = (question) => {
-      return new Promise((resolve) => {
-          rl.question(question, (answer) => {
-              resolve(answer.trim());
-          });
-      });
-  }
+// aks a question
+const promptUser = (question) => {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer.trim());
+        });
+    });
+}
 
-  // variables
-  const currentYear = new Date().getFullYear();
-  let currentAge;
-  let retirementAge;
-  let yearsTillRetirement;
-  let retirementYear;
+// variables
+const currentYear = new Date().getFullYear();
+let currentAge;
+let retirementAge;
+let yearsTillRetirement;
+let retirementYear;
 
-  //function, that returns an async function, that asks a specific year and validate the range
-  const askAge = (question, topic, min, max = 120) => {
-      return async function () {
+// function that returns an async function to ask for a specific age and validate
+const askAge = (question, topic, min, max = 120) => {
+    return async function () {
         let input;
         let age;
         
@@ -32,10 +34,10 @@ const rl = readline.createInterface({
             try {
                 input = await promptUser(question);
                 age = Number(input);
-    
+
                 //validation
                 if (isNaN(age)) {
-                    console.log(`Not a valid ${topic} format`);
+                    console.log(`The input expects a number.`);
                 } else if (age < min || age > max) {
                     console.log(`The ${topic} should be between ${min} and ${max}.`); 
                 }    
@@ -45,19 +47,17 @@ const rl = readline.createInterface({
         } while (isNaN(age) || age < min || age > max);
             
         return age;
-      };  
-  };
+    };  
+};
 
-  const main = async () => {
+const main = async () => {
 
-    const askCurrentAge = askAge("Please enter your age? ", "age", 1, 100);
-  
-    // prompt for current age
+    const askCurrentAge = askAge("Please enter your age: ", "age", 1, 100);
+
     currentAge = await askCurrentAge();
-  
-    const askRetirementAge = askAge("Your retirement age? ", "retirement age", currentAge, 100);
 
-    // prompt for retirement age
+    const askRetirementAge = askAge("At what age do you want to retire? ", "retirement age", currentAge, 120);
+
     retirementAge = await askRetirementAge();
 
     rl.close();
@@ -65,21 +65,31 @@ const rl = readline.createInterface({
     // calculating years until retirement
     yearsTillRetirement = retirementAge - currentAge;
 
-    // calcilating the year of retirement
+    // calculating the year of retirement
     retirementYear = currentYear + yearsTillRetirement;
 
-    console.log('********************************************')
-    console.log(`You have ${yearsTillRetirement} years left until you can retire.`);
-    console.log(`It is ${currentYear}, so you can retire in ${retirementYear}.`);
-    console.log('Thank you for using this program');
+    console.log('********************************************');
+
+    if (yearsTillRetirement === 1) {
+        console.log(`You have ${yearsTillRetirement} year left until you can retire.`);
+    } else if (yearsTillRetirement === 0) {
+        console.log(`You are retiring in the current year, ${retirementYear}.\n`);
+    } else {
+        console.log(`You have ${yearsTillRetirement} years left until you can retire.`);
+    }
+
+    if (currentYear !== retirementYear) {
+        console.log(`It is ${currentYear}, so you can retire in ${retirementYear}.\n`);
+    }
+    
+    console.log('********************************************');
+    console.log('Thank you for using this program!');
 
     return;
-  }
- 
-  main();
+}
 
+main();
 
-// Retirement calculator
 // Create a program that determines how many years you have left until retirement and the year you can retire.
 // It should prompt for your current age and the age you want to retire and display the output as shown in the
 // example below.
